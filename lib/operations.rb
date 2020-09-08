b59e2baa309c113a4ed87c391c742b26ae6e9c5a
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-08 18:20:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
- speed < 40 || speed > 60 ? true : !true
+
 
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : !true
+	speed < 40 || speed > 60 ? true : !true
 end
